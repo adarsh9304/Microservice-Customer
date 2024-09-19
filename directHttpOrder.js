0ef8ca554367ccdef7ee95b1ea9b46a508ed8fa1
adarsh9304 <158045876+adarsh9304@users.noreply.github.com>
2024-09-19 17:01:05 +0530

    Update directHttpOrder.js

--- directHttpOrder.js
+++ directHttpOrder.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const ORDER_SERVICE_URL = 'http://172-31-4-161:3002';  // URL of the Order service
+const ORDER_SERVICE_URL = 'http://3.110.108.19:3002';  // URL of the Order service
 async function getOrderFromOrderService() {
   try {
     const response = await axios.get(`${ORDER_SERVICE_URL}/order-temp`,);
